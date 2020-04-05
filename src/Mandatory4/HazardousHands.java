02064f16d3f41c9aff283e57880716c50df417fd
AngelofDeath666 <khejser@hotmail.com>
2020-04-05 19:34:42 +0200

    Oy blin it's done with Johannes Help

--- src/Mandatory4/HazardousHands.java
+++ src/Mandatory4/HazardousHands.java
@@ -1,97 +1,72 @@
 package Mandatory4;
+
 import java.util.LinkedList;
 import java.util.*;
 
 public class HazardousHands {
+    public static void main(String[] args) {
+        // input to output
+        Scanner sc = new Scanner(System.in);
+        //get virus carrier
+        int carrier = sc.nextInt();
+
+        //count of conference goers
+        int conPeople = sc.nextInt();
+
+        //handshakes made
+        int handshakes = sc.nextInt();
+
+        VirusCarriers virusCarriers = new VirusCarriers(conPeople);
+        for (int i = 0; i < handshakes; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            virusCarriers.addEdge(a, b);
+        }
+        sc.close();
+        virusCarriers.DFSRecursion(carrier);
+
+    }
 
     static class VirusCarriers {
         int vertices;
-        int chain;
+        HashSet r = new HashSet();
         LinkedList<Integer>[] list;
 
 
-        VirusCarriers(int vertices){
+        VirusCarriers(int vertices) {
             this.vertices = vertices;
             list = new LinkedList[vertices];
-            for (int i = 0; i <vertices ; i++) {
+            for (int i = 0; i < vertices; i++) {
                 list[i] = new LinkedList<>();
             }
         }
 
 
-    public void addEdge(int source, int destination) {
-        //add edge
-        list[source].addFirst(destination);
+        public void addEdge(int source, int destination) {
+            //add edge
+            list[source].addFirst(destination);
 
-        //add back edge (for undirected)
-        list[destination].addFirst(source);
+            //add back edge (for undirected)
+            list[destination].addFirst(source);
 
-    }
+        }
 
-    public void DFSRecursion(int startVertex){
-        boolean[] visited = new boolean[vertices];
-        dfs(startVertex, visited);
-    }
+        public void DFSRecursion(int startVertex) {
+            boolean[] visited = new boolean[vertices];
+            dfs(startVertex, visited);
+            System.out.println(r.size());
+        }
 
-    public void dfs(int start, boolean[] visited){
-            chain = 0;
-        visited[start] = true;
-        for (int i = 0; i < list[start].size(); i++){
-            int destination = list[start].get(i);
-            if(!visited[destination]){
-                dfs(destination,visited);
+        public void dfs(int start, boolean[] visited) {
+
+            visited[start] = true;
+            for (int i = 0; i < list[start].size(); i++) {
+                int destination = list[start].get(i);
+                if (!visited[destination]) {
+                    dfs(destination, visited);
+                }
+                r.add(destination);
             }
-            chain += 1;
-            System.out.println(chain);
         }
-    }
-
-
-    public static void main(String[] args){
-        // input to output
-        Scanner sc = new Scanner(System.in);
-
-        //get virus carrier
-        String virusCarrier = sc.nextLine();
-        int carrier = Integer.parseInt(virusCarrier);
-
-        //count of conference goers
-        String conferenceGoers = sc.nextLine();
-        int conPeople = Integer.parseInt(conferenceGoers);
-
-        //handshakes made
-        String handshakesMade = sc.nextLine();
-        int handshakes = Integer.parseInt(handshakesMade);
-
-        ArrayList<String> blin = new ArrayList<>();
-        String[] tokens = new String[2];
-        int[] blyat = new int[tokens.length];
-
-        for (int i = 0 ; i < handshakes ; i++){
-            tokens = sc.nextLine().split("\\s");
-            blin.addAll(Arrays.asList(tokens));
-            for (int j = 0; j < blin.size(); j++ ){
-                blyat[j] = Integer.parseInt(blin.get(i));
-                System.out.println(Arrays.toString(blyat)); //todo delete
-            }
-
-            System.out.println(Arrays.toString(tokens)); //todo delete
-
-
-        }
-        sc.close();
-
-        int vertices = conPeople;
-        VirusCarriers virusCarriers = new VirusCarriers(vertices);
-        for (int i = 0 ; i < (blyat.length/2); i++){
-            virusCarriers.addEdge(blyat[i],blyat[i+1]);
-            i =+1;
-
-        }
-
-        virusCarriers.DFSRecursion(carrier);
-
-        }
-
     }
 }
